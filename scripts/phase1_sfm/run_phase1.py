a828196fb4d5c95c4201d204336c4e4d27f62857
tabsanro <dojan789@sju.ac.kr>
2026-05-18 11:10:46 +0900

    LGPL v2.1 라이선스 추가

--- scripts/phase1_sfm/run_phase1.py
+++ scripts/phase1_sfm/run_phase1.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+# This file is part of DT_SERVER.
+# 
+# DT_SERVER is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) any later version.
+# 
+# DT_SERVER is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with DT_SERVER; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
 """
 Phase 1 실행 스크립트 - SfM 맵 검증 및 고정 카메라 PnP 추정 테스트
 
